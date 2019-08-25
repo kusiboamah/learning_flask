b367c6f0e51c8e2ae82280c6bf8b0ec26b8623da
Rangeroverk7 <rangeroverk7@gmail.com>
2019-08-25 09:03:07 +0000

    Routes changes

--- forms.py
+++ forms.py
@@ -1,5 +1,7 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
 
 class SignupForm(Form):
     first_name = StringField('First Name')
@@ -7,3 +9,8 @@ class SignupForm(Form):
     email = StringField('Email')
     password = PasswordField('Password')
     submit = SubmitField('Sign Up')
+    
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter a password.")])
+    submit = SubmitField("Sign in")
