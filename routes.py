e730a0840f3e25bf521a4c1a558853050ccbd57f
Rangeroverk7 <rangeroverk7@gmail.com>
2019-08-27 13:08:31 +0000

    postgres database-uri

--- routes.py
+++ routes.py
@@ -5,7 +5,7 @@ from forms import SignupForm, LoginForm
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://DATABASE_URI'
 db.init_app(app)
 
 
